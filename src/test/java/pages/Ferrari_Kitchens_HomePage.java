b2ec67fbdf65018e955adb082722bcc2943f06ee
Henry Leu <mingjenleu@gmail.com>
2022-04-03 11:56:24 -0400

    update

--- src/test/java/pages/Ferrari_Kitchens_HomePage.java
+++ src/test/java/pages/Ferrari_Kitchens_HomePage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utililties.BaseClass;
 
@@ -9,6 +11,17 @@ public class Ferrari_Kitchens_HomePage {
 	public Ferrari_Kitchens_HomePage() {
 		PageFactory.initElements(BaseClass.getDriver(), this);
 	}
+	
+	// Risa test case #, story #
+	@FindBy(xpath = "dsfas")
+	private WebElement loginBtn;
+
+	// Risa test case #, story #
+	public WebElement getLoginBtn() {
+		return loginBtn;
+	}
+	
+	
 	
 
 }
