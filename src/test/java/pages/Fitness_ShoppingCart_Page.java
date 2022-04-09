805f765a0539234792331f8e672698b37a03a1f9
AimNetis <aimjirapa@gmail.com>
2022-04-09 01:01:58 -0400

    Aim US005 Fitness

--- src/test/java/pages/Fitness_ShoppingCart_Page.java
+++ src/test/java/pages/Fitness_ShoppingCart_Page.java
@@ -1,16 +1,47 @@
 package pages;
 
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utililties.BaseClass;
 
-
 public class Fitness_ShoppingCart_Page {
-	
+
 	public Fitness_ShoppingCart_Page() {
 		PageFactory.initElements(BaseClass.getDriver(), this);
+
 	}
-	
+
+	// US005_Fitness_Aim
+	@FindBy(xpath = "//div[@id='header']/div[4]/span[1]/span[1]")
+	public WebElement welcomeGuest;
+
+	// US005_Fitness_Aim
+	@FindBy(xpath = "//div[@id='main-menu-navigation-menu-collapse']//li[8]/a")
+	public WebElement clickNewArrivals;
+
+	// US005_Fitness_Aim
+	@FindBy(xpath = "//div[@class='widget product_list grid']//button[not(@style='display: none;')]")
+	public List<WebElement> listOfItemsEbikes;
+
+	@FindBy(xpath = "//h2[text()='New Arrivals']//parent::div//following-sibling::div/div")
+	public List<WebElement> listOfNewArrivals;
+
+	// US005_Fitness_Aim
+	@FindBy(xpath="//div[10]//div[5]//div[1]//form[1]//div[1]//button[1]")
+	public  WebElement addToCartButton;
+
+	// US005_Fitness_Aim
+	@FindBy(xpath = "//div[@id='main-menu-navigation-menu-collapse']/ng-include/ul/li[1]/a")
+	public WebElement clickOnEbikes;
+
+	// US005_Fitness_Aim
+	@FindBy(xpath = "//*[@id='sidebar_cart_preview']/div[2]/div[3]/button")
+	public WebElement checkoutButton;
 
 	
-
+	
+	
 }
--- src/test/java/runners/BeatTheBootCampRunner.java
+++ src/test/java/runners/BeatTheBootCampRunner.java
@@ -4,19 +4,16 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+//@RunWith(Cucumber.class)
+//@CucumberOptions(		
 
 @RunWith(Cucumber.class)
-@CucumberOptions(		
-		plugin = {	"pretty", "json:target/JSON_Reports/Cucumber.json",
-				"junit:target/Cucumber_Reports/Cucumber.xml",
-		"html:target/HTML_Reports/index.html"},
-		dryRun = false,
-		features = {"src/test/resources/features"},
-		glue = {"stepdefinitions"},
-		monochrome = true
-		)
+@CucumberOptions(plugin = { "pretty", "json:target/JSON_Reports/Cucumber.json",
+		"junit:target/Cucumber_Reports/Cucumber.xml", "html:target/HTML_Reports/index.html" }, dryRun = false,
+//		features = {"src/test/resources/features/US005_Fitness_By_Aim.feature/"},
+		features = { "src/test/resources/features/" }, glue = { "stepdefinitions" }, monochrome = true)
 public class BeatTheBootCampRunner {
 
-	public BeatTheBootCampRunner() {} 
+//	public BeatTheBootCampRunner() {} 
 
 }
