a0d34ac9a69e9f343502da1fcc76d496ca80090c
siriwan saitia <ling.siriwan@gmail.com>
2022-04-10 14:06:13 -0400

    Siri Testcase 001 Ferrari

--- src/test/java/pages/Ferrari_Kitchens_HomePage.java
+++ src/test/java/pages/Ferrari_Kitchens_HomePage.java
@@ -1,27 +1,58 @@
 package pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utililties.BaseClass;
 
-
 public class Ferrari_Kitchens_HomePage {
-	
+
 	public Ferrari_Kitchens_HomePage() {
 		PageFactory.initElements(BaseClass.getDriver(), this);
 	}
-	
-	// Risa test case #, story #
-	@FindBy(xpath = "dsfas")
-	private WebElement loginBtn;
 
-	// Risa test case #, story #
-	public WebElement getLoginBtn() {
-		return loginBtn;
+	// Siri Test case 001_FerrariKitchens_Story1
+	@FindBy(xpath = "//div[@class='tp-loop-wrap']")
+	public List<WebElement> fourSliders;
+
+	// Siri Test case 001_FerrariKitchens_Story2
+	@FindBy(xpath = "//div[@id='rev_slider_4_1']")
+	public WebElement homepageImage;
+
+	// Siri Test case 001_FerrariKitchens_Story3
+	// US001_Ferrari - Siri
+	@FindBy(xpath = "//div[@class='tp-leftarrow tparrows hesperiden']")
+	private WebElement leftArrow;
+
+	// US001_Ferrari - Siri
+	@FindBy(xpath = "//div[@class='tp-rightarrow tparrows hesperiden']")
+	private WebElement rightArrow;
+
+	// US001_Ferrari - Siri
+	@FindBy(xpath = "//div[@id='rev_slider_4_1']/ul/li//div[contains(@class,'img')]//following::div[@class='tp-caption slidelink']")
+	private List<WebElement> images;
+
+	// US001_Ferrari - Siri
+	public WebElement getLeftArrow() {
+		return leftArrow;
 	}
-	
-	
-	
+
+	// US001_Ferrari - Siri
+	public WebElement getRightArrow() {
+		return rightArrow;
+	}
+
+	// US001_Ferrari - Siri
+	public List<WebElement> getImages() {
+		return images;
+	}
+
+	// US001_Ferrari - Siri
+	public int getID(WebElement string) {
+		int id = Integer.parseInt(string.getAttribute("id").replaceAll("[^0-9]", ""));
+		return id;
+	}
 
 }
--- src/test/java/utililties/CommonMethods.java
+++ src/test/java/utililties/CommonMethods.java
@@ -1,5 +1,73 @@
 package utililties;
 
-public class CommonMethods extends PageInitializer{
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CommonMethods extends PageInitializer {
+
+	// TC01_USER003 SIRI
+	public static void schrollDown() {
+		JavascriptExecutor js = (JavascriptExecutor) getDriver();
+		js.executeScript("window.scrollBy(0,300)", "");
+
+	}
+
+	public static WebDriverWait getWaitObject() {
+		WebDriverWait wait = new WebDriverWait(getDriver(), 30);
+		return wait;
+	}
+
+	public static List<WebElement> waitForAllElementsToBeVisible(List<WebElement> element) {
+		return getWaitObject().until(ExpectedConditions.visibilityOfAllElements(element));
+	}
+
+	public static WebElement waitForVisibility(WebElement element) {
+		return getWaitObject().until(ExpectedConditions.visibilityOf(element));
+	}
+
+	public static WebElement waitForClickability(WebElement element) {
+		return getWaitObject().until(ExpectedConditions.elementToBeClickable(element));
+	}
+
+	public static void click(WebElement element) {
+		waitForClickability(element);
+		element.click();
+	}
+
+	public static byte[] takeScreenshot(String filename) {
+		TakesScreenshot ts = (TakesScreenshot) getDriver();
+		byte[] picBytes = ts.getScreenshotAs(OutputType.BYTES);
+
+		File file = ts.getScreenshotAs(OutputType.FILE);
+		String destinationFile = "" + filename + getTimeStemp() + ".png";
+
+		try {
+			FileUtils.copyFile(file, new File(destinationFile));
+		} catch (Exception ex) {
+			System.out.println("Cannot take screenshot!");
+		}
+
+		return picBytes;
+	}
+
+	public static String getTimeStemp() {
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+		return sdf.format(date.getTime());
+	}
+
+	// TC03_USER001 - Siri
+	public static Boolean waitForInvisibility(WebElement element) {
+		return getWaitObject().until(ExpectedConditions.invisibilityOf(element));
+	}
 
 }
