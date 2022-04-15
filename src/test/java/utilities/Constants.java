6af157bcfdc91ae22018f65bbc8714154e2e733a
Henry Leu <mingjenleu@gmail.com>
2022-04-14 23:03:47 -0400

    update

--- src/test/java/utilities/Constants.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class Constants {
-	public static final int IMPLICIT_WAIT_TIME=10;
-	public static final int EXPLICIT_WAIT_TIME=30;
-	public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir") + "/src/test/resources/datasource/config.properties";
-}
